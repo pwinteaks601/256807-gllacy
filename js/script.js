f583b988e32d90906ccebe0870f99f61ff77f310
Maria Diakova <pwinteaks601@gmail.com>
2018-03-29 22:53:48 +0300

    Все исправила, минифицировала стили и скрипты

--- js/script.js
+++ js/script.js
@@ -14,3 +14,18 @@ close.addEventListener("click", function (evt) {
   evt.preventDefault ()
   popup.classList.remove("modal-show");
 });
+
+function initMap() {
+        var place = {lat: 59.9387942, lng: 30.3230833};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 16,
+          center: {lat: 59.938693, lng: 30.328756}
+        });
+
+        var image = 'img/pin.svg';
+        var marker = new google.maps.Marker({
+          position: place,
+          map: map,
+          icon: image
+        });
+      }
